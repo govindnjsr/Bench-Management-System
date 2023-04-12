30f0bec1338a9f93627425c5bd82c3a3e47002d6
govindnjsr <76946530+govindnjsr@users.noreply.github.com>
2023-04-12 13:10:25 +0530

    Delete Backend directory

--- Backend/src/main/java/com/example/Bench/Management/Project/Repository/InterviewRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Bench.Management.Project.Repository;
-
-import com.example.Bench.Management.Project.Model.IntDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InterviewRepo extends JpaRepository<IntDetails,Long> {
-}
