ac519ca268b40397834df8e2be211c2b5f011721
dhruvBansal0809 <dhruv.bansal@accolitedigital.com>
2023-04-15 04:15:42 +0530

    viewEmployee updated

--- Frontend/bench-management/src/components/AuthState.js
+++ Frontend/bench-management/src/components/AuthState.js
@@ -1,58 +1,76 @@
-import React from 'react'
-import AuthContext from './AuthContext'
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import AuthContext from "./AuthContext";
+import { useState } from "react";
 export default function AuthState(props) {
-
     const [isAuthentication, setAuthentication] = useState(false);
-    const[currentRole,setCurrentRole]=useState(0)
-    const[googleData,setGoogleData]=useState({})
-    const[dtoData,setDtoData]=useState()
-    const[checkFilter,setCheckFilter]=useState({
-        "skill":false,
-        "location":false,
-        "status":false
-    })
-    const[appliedFilters,setAppliedFilters]=useState({
-        "experience":false,
-        "benchtime":false,
-        "java":false,
-        "python":false,
-        "react":false,
-        "angular":false,
-        "html":false,
-        "css":false,
-        "javascript":false,
-        "springboot":false,
-        "gurugram":false,
-        "bangalore":false,
-        "hyderabad":false,
-        "active":false,
-        "benched":false
-    })
+    const [currentRole, setCurrentRole] = useState(0);
+    const [googleData, setGoogleData] = useState({});
+    const [dtoData, setDtoData] = useState();
+    const [checkFilter, setCheckFilter] = useState({
+        skill: false,
+        location: false,
+        status: false,
+    });
+    const [appliedFilters, setAppliedFilters] = useState({
+        experience: false,
+        benchtime: false,
+        java: false,
+        python: false,
+        react: false,
+        angular: false,
+        html: false,
+        css: false,
+        javascript: false,
+        springboot: false,
+        gurugram: false,
+        bangalore: false,
+        hyderabad: false,
+        active: false,
+        benched: false,
+    });
 
     const [managerId, setManagerId] = useState(-1);
     const [loopEntry, setLoopEntry] = useState(false);
     const [viewEmployeeId, setViewEmployeeId] = useState(0);
     const handleEmpId = (id) => {
         setViewEmployeeId(id);
-      }
+    };
     function handleLogin() {
-        setAuthentication(true)
-        console.log("logged in Successfully")
+        setAuthentication(true);
+        console.log("logged in Successfully");
     }
     function handleLogout() {
-        setAuthentication(false)
-        setCurrentRole(0)
-        setLoopEntry(false)
-        console.log("logged out successfully")
+        setAuthentication(false);
+        setCurrentRole(0);
+        setLoopEntry(false);
+        console.log("logged out successfully");
     }
     return (
-        <AuthContext.Provider value={{checkFilter,setCheckFilter,appliedFilters,dtoData, googleData, setAppliedFilters,setDtoData,isAuthentication, currentRole,
-         setGoogleData, handleLogin, setAuthentication,
-          setCurrentRole, handleLogout, loopEntry,setLoopEntry, managerId, setManagerId
-          ,handleEmpId, viewEmployeeId }}>
+        <AuthContext.Provider
+            value={{
+                checkFilter,
+                setCheckFilter,
+                appliedFilters,
+                dtoData,
+                googleData,
+                setAppliedFilters,
+                setDtoData,
+                isAuthentication,
+                currentRole,
+                setGoogleData,
+                handleLogin,
+                setAuthentication,
+                setCurrentRole,
+                handleLogout,
+                loopEntry,
+                setLoopEntry,
+                managerId,
+                setManagerId,
+                handleEmpId,
+                viewEmployeeId,
+            }}
+        >
             {props.children}
         </AuthContext.Provider>
-    )
+    );
 }
