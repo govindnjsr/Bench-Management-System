21b482bd43963afccefb051bc2deb4edf4b9ed94
meghaMathur <megha.mathur@accolitedigital.com>
2023-04-24 21:28:01 +0530

    integrated with search bar

--- Frontend/bench-management/src/components/ViewReport.js
+++ Frontend/bench-management/src/components/ViewReport.js
@@ -1,6 +1,6 @@
 import { Bar } from "react-chartjs-2";
-import { Category } from "react-chartjs-2";
-import Chart from "chart.js/auto";
+// import { Category } from "react-chartjs-2";
+// import Chart from "chart.js/auto";
 import NavBar from "./Navbar";
 import "./Project.css";
 import React, { Component } from "react";
