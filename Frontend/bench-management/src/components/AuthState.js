894d4019cea8eca9dfe90557b444f4186b7d0b3f
dhruvBansal0809 <dhruv.bansal@accolitedigital.com>
2023-04-24 14:42:40 +0530

    fixed search bar and bench ageging

--- Frontend/bench-management/src/components/AuthState.js
+++ Frontend/bench-management/src/components/AuthState.js
@@ -1,46 +1,47 @@
 import React from 'react'
 import AuthContext from './AuthContext'
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 export default function AuthState(props) {
 
-    
+
     const [isAuthentication, setAuthentication] = useState(false);
-    const[currentRole,setCurrentRole]=useState(0)
-    const[googleData,setGoogleData]=useState({})
-    const[dtoData,setDtoData]=useState()
-    const [benchTimeValue, setBenchTimeValue] = useState(1);
-    const [experienceValue, setExperienceValue] = useState(1);
+    const [currentRole, setCurrentRole] = useState(0)
+    const [googleData, setGoogleData] = useState({})
+    const [dtoData, setDtoData] = useState()
+    const [benchTimeValue, setBenchTimeValue] = useState(0);
+    const [experienceValue, setExperienceValue] = useState(0);
     const [show, setShow] = useState(false);
     const [post, setPost] = useState()
-    const[assignedLocation,setAssignedLocation]=useState({})
-    const[checkFilter,setCheckFilter]=useState({
-        "skill":0,
-        "location":0,
-        "status":0
+    const [searchValue, setSearchValue] = useState("");
+    const [assignedLocation, setAssignedLocation] = useState({})
+    const [benchPeriodEmp, setBenchPeriodEmp] = useState(0);
+    const [checkFilter, setCheckFilter] = useState({
+        "skill": 0,
+        "location": 0,
+        "status": 0
     })
-    const[locationAcess,setLocationAccess]=useState({
-        "gurugram":true,
-        "hyderabad":true,
-        "bangalore":true
-        
+    const [locationAcess, setLocationAccess] = useState({
+        "gurugram": true,
+        "hyderabad": true,
+        "bangalore": true
+
     })
-    const[appliedFilters,setAppliedFilters]=useState({
-        "experience":false,
-        "benchtime":false,
-        "java":false,
-        "python":false,
-        "react":false,
-        "angular":false,
-        "html":false,
-        "css":false,
-        "javascript":false,
-        "springboot":false,
-        "gurugram":false,
-        "bangalore":false,
-        "hyderabad":false,
-        "active":false,
-        "benched":false
+    const [appliedFilters, setAppliedFilters] = useState({
+        "experience": false,
+        "benchtime": false,
+        "java": false,
+        "python": false,
+        "react": false,
+        "angular": false,
+        "html": false,
+        "css": false,
+        "javascript": false,
+        "springboot": false,
+        "gurugram": false,
+        "bangalore": false,
+        "hyderabad": false,
+        "active": false,
+        "benched": false
     })
     const handleClose = () => {
         setShow(false);
@@ -51,7 +52,7 @@ export default function AuthState(props) {
     const [viewEmployeeId, setViewEmployeeId] = useState(0);
     const handleEmpId = (id) => {
         setViewEmployeeId(id);
-      }
+    }
     function handleLogin() {
         setAuthentication(true)
         console.log("logged in Successfully")
@@ -63,13 +64,16 @@ export default function AuthState(props) {
         console.log("logged out successfully")
     }
     return (
-        <AuthContext.Provider value={{locationAcess,setLocationAccess,checkFilter,setCheckFilter,appliedFilters,dtoData, googleData, setAppliedFilters,setDtoData,isAuthentication, currentRole,
-         setGoogleData, handleLogin, setAuthentication,
-          setCurrentRole, handleLogout, loopEntry,setLoopEntry, managerId, setManagerId
-          ,handleEmpId, viewEmployeeId, benchTimeValue, setBenchTimeValue,
-           experienceValue, setExperienceValue, handleClose,
+        <AuthContext.Provider value={{
+            locationAcess, setLocationAccess, checkFilter, setCheckFilter, appliedFilters, dtoData, googleData, setAppliedFilters, setDtoData, isAuthentication, currentRole,
+            setGoogleData, handleLogin, setAuthentication,
+            setCurrentRole, handleLogout, loopEntry, setLoopEntry, managerId, setManagerId
+            , handleEmpId, viewEmployeeId, benchTimeValue, setBenchTimeValue,
+            experienceValue, setExperienceValue, handleClose,
             setShow, show, handleShow, post, setPost,
-            assignedLocation, setAssignedLocation}}>
+            assignedLocation, setAssignedLocation, searchValue,
+            setSearchValue, benchPeriodEmp, setBenchPeriodEmp
+        }}>
             {props.children}
         </AuthContext.Provider>
     )
