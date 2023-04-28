fe41d93df349f0495ddc4be1de2ed909031689ea
meghaMathur <megha.mathur@accolitedigital.com>
2023-04-28 11:24:30 +0530

    commit by megha

--- Frontend/bench-management/src/components/Login.js
+++ Frontend/bench-management/src/components/Login.js
@@ -9,7 +9,7 @@ import ManagerDashboard from './ManagerDashboard';
 import AdminDashboard from './AdminDashboard';
 export default function Login() {
   const authData = useContext(AuthContext);
-  const manager = 1;
+  const manager = 2;
   const [loginApiData, setLoginApiData] = useState();
 
   function handleCallbackResponse(response) {
