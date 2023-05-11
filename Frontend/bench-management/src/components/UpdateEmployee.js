9bdc16fbf1c01ac36ca470cc4a7fbac35b6a534c
Gobind2538 <gobind@accolitedigital.com>
2023-05-11 11:42:34 +0530

    oooooooooooooooo

--- Frontend/bench-management/src/components/UpdateEmployee.js
+++ Frontend/bench-management/src/components/UpdateEmployee.js
@@ -77,8 +77,7 @@ function UpdateEmployee(props) {
                 </Modal.Header>
                 <Modal.Body>
                     <Form id='add'>
-                        <p>Are you sure you want to remove <b>{props.name}</b> from bench?</p>
-                      
+                        <p>Are you sure you want to remove <b>{props.name}</b> from bench?</p>                      
                         {/* <Form.Group>
                             <Form.Label>Bench Status</Form.Label>
                             <Form.Select aria-label="Default select example" name="benchStatus" onChange={handleChangeValue.bind(this)} >
